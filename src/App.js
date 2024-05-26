72dc25ec4e5258c842910555fad76ccadcf14359
bon bon <bona35165@gmail.com>
2024-05-26 22:57:18 +0700

    thirt commit

--- src/App.js
+++ src/App.js
@@ -1,30 +1,20 @@
-import axios from "axios";
-import { useEffect } from "react";
+import React from "react";
+import { Route, BrowserRouter as Router, Switch } from "react-router-dom";
 import "./App.css";
-import Navbar from "./component/layout/Navbar";
-import {useState} from "react";
-import Users from "./component/users/Users";
-// App.js
+import Navbar from "./components/layout/Navbar";
+import Search from "./components/users/Search";
 const App = () => {
-  const [users, setUsers] = useState([]);
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await axios.get("https://api.github.com/users");
-        setUsers(response.data);
-      } catch (error) {
-        console.error("Error fetching data:", error);
-      }
-    };
-    fetchData();
-  }, []);
   return (
     <div className="App">
-      <Navbar />
-      <div className="container">
-        <h1>GitHub Users Data</h1>
-      </div>
-      <Users users={users} />
+      <Router>
+        <Navbar />
+        <div className="container">
+          <h1>GitHub Users Data</h1>
+          <Switch>
+            <Route exact path="/" component={Search} />
+          </Switch>
+        </div>
+      </Router>
     </div>
   );
 };
---
+++ src/components/users/Search.js
@@ -0,0 +1,33 @@
+// Search.js
+import axios from "axios";
+import React, { useState } from "react";
+import Users from "./Users";
+const Search = () => {
+  const [text, setText] = useState("");
+  const [users, setUsers] = useState([]);
+  const searchUsers = async (text) => {
+    // .. Rest of the code
+  };
+  const clearUsers = () => {
+    setUsers([]);
+  };
+  const onSubmit = (e) => {
+    // ... rest of the code
+  };
+  const onChange = (e) => setText(e.target.value);
+  return (
+    <div>
+      <form onSubmit={onSubmit} className="form">
+        {/*<!..> ... The rest of the form */}
+      </form>
+      {/*Adding Clear button */}
+      {users.length > 0 && (
+        <button className="btn btn-danger btn-block" onClick={clearUsers}>
+          Clear
+        </button>
+      )}
+      <Users users={users} />
+    </div>
+  );
+};
+export default Search;
